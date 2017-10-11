5edc0b4933907c262e574393525878624e2e7f7a
exactone <d04921005@ntu.edu.tw>
2017-10-12 00:25:18 +0800

    Update gcp_init.sh
    basic lib for crawler

--- gcp_init.sh
+++ gcp_init.sh
@@ -48,6 +48,8 @@ sedhome=$(echo $HOME | sed 's/\//\\\//g')
 sed -i "s/#c.NotebookApp.certfile = ''/c.NotebookApp.certfile = '$sedhome\/ssl\/mycert.pem'/g"  ~/.jupyter/jupyter_notebook_config.py 
 sed -i "s/#c.NotebookApp.keyfile = ''/c.NotebookApp.keyfile = '$sedhome\/ssl\/mykey.key'/g"  ~/.jupyter/jupyter_notebook_config.py
 sed -i "s/#c.NotebookApp.ip = 'localhost'/c.NotebookApp.ip = '\*'/g"  ~/.jupyter/jupyter_notebook_config.py
+
+
 touch get_sha_passwd.py
 echo "from IPython.lib import passwd" >> get_sha_passwd.py
 echo "print(passwd())" >> get_sha_passwd.py
@@ -56,3 +58,29 @@ rm -f get_sha_passwd.py
 sed -i "s/#c.NotebookApp.password = ''/c.NotebookApp.password = u'$sha1passwd'/g" ~/.jupyter/jupyter_notebook_config.py 
 sed -i "s/#c.NotebookApp.open_browser = True/c.NotebookApp.open_browser = False/g" ~/.jupyter/jupyter_notebook_config.py
 sed -i "s/#c.NotebookApp.port = 8888/c.NotebookApp.port = 9999/g" ~/.jupyter/jupyter_notebook_config.py
+
+echo '=============='
+echo 'install fake_useragent, selenium, '
+echo '=============='
+source ~/.bashrc
+yes | conda create --name crawler anaconda
+source activate crawler
+pip install fake_useragent
+pip install -U selenium
+
+
+echo '=============='
+echo 'install phantomjs'
+echo '=============='
+sudo apt-get update
+sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y
+sudo apt-get install libfreetype6 libfreetype6-dev -y
+sudo apt-get install libfontconfig1 libfontconfig1-dev -y
+cd ~
+export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
+wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2
+sudo tar xvjf $PHANTOM_JS.tar.bz2
+sudo mv $PHANTOM_JS /usr/local/share
+sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
+
+source deactivate
