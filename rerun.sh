095d1226f7b649c997a530a721184c041db9a4ce
exactone <d04921005@ntu.edu.tw>
2017-10-14 04:30:38 +0000

    bug fix

--- rerun.sh
+++ rerun.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 cd ..
 mv findbiz_crawler findbiz_crawler_$1
+git clone https://github.com/exactone/findbiz_crawler
 cp -f findbiz_crawler_$1/all_json_out_json.json findbiz_crawler/.
-cp -f findbiz_crawler_$1/aall_json_out_json.json all_json_out_json.json.bk$1
+cp -f findbiz_crawler_$1/all_json_out_json.json all_json_out_json.json.bk$1
 rm -rf findbiz_crawler_$1/
 cd findbiz_crawler/
+source activate crawler
 python crawler_v10.py $2 /usr/local/bin/phantomjs $1 1
