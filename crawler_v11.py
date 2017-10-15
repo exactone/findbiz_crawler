ce38927f45e3abddb1f32ecd15c1c4e4f36d95a9
exactone <d04921005@ntu.edu.tw>
2017-10-15 11:09:11 +0800

    Update crawler_v11.py
    bug fix

--- crawler_v11.py
+++ crawler_v11.py
@@ -1688,7 +1688,7 @@ class parser_cmpy_type:
 
 # In[7]:
 
-path_phantomjs = sys.argv[2]
+path_phantomjs = sys.argv[1]
 #path_phantomjs = '/usr/local/Cellar/phantomjs/2.1.1/bin/phantomjs'
 
 task = [
